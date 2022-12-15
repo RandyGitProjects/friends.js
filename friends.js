869258cbff27bc6ee3d95e727f6c1a5223b4faf1
Randy Cisneros <rlcisneros90@gmail.com>
2022-12-15 10:55:24 -0700

    Add second array

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var array = ["name1", "name2", "name3", "name4", 'name5']
 console.log(array[0]);
 console.log(array[2]);
 console.log(array[4]);
+
+var array2 = ["New York", "Japan", "Spain", "Bali", "Singapore"]
+console.log(array2.push("Paris"))
+console.log(array2)
