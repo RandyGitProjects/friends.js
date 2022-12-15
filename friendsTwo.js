d96cff2ff576504d5a8313dff6a428226f01fd6a
Randy Cisneros <rlcisneros90@gmail.com>
2022-12-15 11:54:54 -0700

    Add second if/else statement

--- friendsTwo.js
+++ friendsTwo.js
@@ -1,7 +1,20 @@
 var bestFriend = ("Brooke")
 var otherFriend = ("Jared")
-if (bestFriend === "Brooke") {
-    console.log("Hello, best friend");
-} else {
-    console.log("Hello, other friend")
-}
+var anotherFriend = ("Luis")
+// if (bestFriend === "Brooke") {
+//     console.log("Hello, best friend"); {
+//     } elseif (anotherFriend === "Luis") {
+//         console.log("Hello, friend"); {
+//     } else {
+//         console.log("Hello, other friend");
+// }
+// }
+
+if (bestFriend === "Rawr") {
+    console.log("Hello, best friend"); 
+}   else if (anotherFriend === "Luis") { 
+    console.log("hello, friend"); 
+}   else 
+    console.log("Hello, other friend");
+        
+    
